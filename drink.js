baf6e5df551596aeb496d1422dd5488b9ac8513d
Derek James <derekjames1093@gmail.com>
2020-09-23 16:01:54 -0500

    day2

--- drink.js
+++ drink.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function(event){
     // create prevent defeault
     event.preventDefault();
     //fetch data for eventbrite api to put in iframe
-    fetch('https://www.eventbriteapi.com/v3/events/120067319585/?token=BZU2AGMQK7AESWXYITBJ')
+    fetch('https://www.eventbriteapi.com/v3/events/115883487631/?token=BZU2AGMQK7AESWXYITBJ')
     // return api response to a json
     .then(function(response){
         return response.json();
@@ -23,33 +23,31 @@ document.addEventListener('DOMContentLoaded', function(event){
         const link = data.url
         iFrame.setAttribute('src',link)
     })
-    
 
 })
-
+    
 
 
 btn1.addEventListener('click', function(event){
-    // create prevent defeault
     event.preventDefault();
-    
 
 
-    fetch('https://www.eventbriteapi.com/v3/events/120067319585/?token=BZU2AGMQK7AESWXYITBJ')
+    fetch('https://www.eventbriteapi.com/v3/events/115883487631/?token=BZU2AGMQK7AESWXYITBJ')
         .then(function(response){
             return response.json()
         })
         .then(function(data){ 
             console.log(data.name.text)
             
-            fetch(`https://www.googleapis.com/youtube/v3/search/?part=snippet&q=${data.name.text}&key=AIzaSyBw7ldeQl94AT_dRmGV5mWjupsyaq2pBfI`)
+            fetch(`https://www.googleapis.com/youtube/v3/search/?part=snippet&q=${data.name.text}&key=AIzaSyC26dSjpIa0W81IXM3L9YOwbnOtqZJ3QaA`)
             .then(function(response){
                 return response.json()
             })
             .then(function(data){
                 data.items.forEach(video => {
-                    console.log(video.id.videoId)
+                    console.log(video.snippet.thumbnails.high.url)
                 });
             })
         })
+    
 })
--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const btn2 = document.querySelector('.btn2')
 const btn3 = document.querySelector('.btn3')
 const btn4 = document.querySelector('.btn4')
 const btn5 = document.querySelector('.btn5')
+const btn5 = document.querySelector('.btn6')
 
 
 btn1.addEventListener('click', function(event){
@@ -18,17 +19,22 @@ btn2.addEventListener('click', function(event){
 btn3.addEventListener('click', function(event){
     console.log('hello world')
     event.preventDefault();
-    window.location.href = "./pizza.html"
+    window.location.href = "./drink.html"
 })
 btn4.addEventListener('click', function(event){
     console.log('hello world')
     event.preventDefault();
-    window.location.href = "./pizza.html"
+    window.location.href = "./pasta.html"
 })
 btn5.addEventListener('click', function(event){
     console.log('hello world')
     event.preventDefault();
     window.location.href = "./pizza.html"
+})
+btn6.addEventListener('click', function(event){
+    console.log('hello world')
+    event.preventDefault();
+    window.location.href = "./index.html"
 })
 
 
