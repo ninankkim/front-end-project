c897c66de6d861c31b9ef40035d11978748d501f
Andrew Pham <andrewpham92@gmail.com>
2020-09-25 10:23:53 -0400

    Refactor the js to async function

--- pasta.js
+++ pasta.js
@@ -1,53 +1,51 @@
-const btn1 = document.querySelector('.btn1')
+const eventBriteApi = 'https://www.eventbriteapi.com/v3/events/109703516170/?token=BZU2AGMQK7AESWXYITBJ'
 
-document.addEventListener('DOMContentLoaded', function(event){
-  // code to load iframe when page loads
-  let iFrame = document.createElement('iframe');
+async function loadEventBrite () {
+    let iFrame = document.createElement('iframe');  
+    iFrame.setAttribute('id','iFrame');
+    iFrame.height = '600'
+    iFrame.width = '600'
+    let divIframe = document.querySelector('#divIframe')
+    divIframe.appendChild(iFrame)
+    const response = await fetch(eventBriteApi)
+    const data = await response.json()
+    const link = await data.url
+    iFrame.setAttribute('src', link)
+    return data;
+}
 
-  
-  iFrame.setAttribute('id','iFrame');
-  iFrame.height = '600'
-  iFrame.width = '600'
-  let divIframe = document.querySelector('#divIframe')
-  divIframe.appendChild(iFrame)
-  // create prevent defeault
-  event.preventDefault();
-  //fetch data for eventbrite api to put in iframe
-  fetch('https://www.eventbriteapi.com/v3/events/109703516170/?token=BZU2AGMQK7AESWXYITBJ')
-  // return api response to a json
-  .then(function(response){
-      return response.json();
-  })
-  .then(function(data){
-      console.log(data.url)
-      const link = data.url
-      iFrame.setAttribute('src',link)
-  })
-})
-  
+async function loadVideoData () {
+    const response1 = await fetch (eventBriteApi);
+    const data = await response1.json();
+    const youtubeData = `https://www.googleapis.com/youtube/v3/search/?part=snippet&q=${data.name.text}&key=AIzaSyA4oLid5kOieJZhkWcUb2zc0hM3t_vjusA
+`
+    const response2 = await fetch(youtubeData);
+    const data2 = await response2.json();
+    const video = data2.items[0];
+    return video;
+}
+
+function addVideo (video) {
+    let videoFrame = document.createElement('iframe')
+    videoFrame.setAttribute('id','iFrame');
+    videoFrame.height = '600'
+    videoFrame.width = '600'
+    let divIframe2 = document.querySelector('#divIframe2')
+    divIframe2.height = '400'
+    divIframe2.width = '400'
+    divIframe2.appendChild(videoFrame)
+    let youTube = `https://www.youtube.com/embed/${video.id.videoId}`
+    videoFrame.setAttribute('src',youTube)
+    let p1 = document.createElement('p');           
+    let titleDescrip = video.snippet.description             
+    p1.textContent = titleDescrip              
+    divIframe2.appendChild(p1)
+}
 
 
-btn1.addEventListener('click', function(event){
-  event.preventDefault();
-  
 
-
-    fetch('https://www.eventbriteapi.com/v3/events/109703516170/?token=BZU2AGMQK7AESWXYITBJ')
-        .then(function(response){
-            return response.json()
-        })
-        .then(function(data){ 
-            console.log(data.name.text)
-            
-            fetch(`https://www.googleapis.com/youtube/v3/search/?part=snippet&q=${data.name.text}&key=AIzaSyC26dSjpIa0W81IXM3L9YOwbnOtqZJ3QaA`)
-            .then(function(response){
-                return response.json()
-            })
-            .then(function(data){
-                data.items.forEach(video => {
-                    console.log(video.snippet.thumbnails.high.url)
-                });
-            })
-        })
-    
+document.addEventListener('DOMContentLoaded', async () => {
+    loadEventBrite()
+    const video = await loadVideoData()
+    addVideo(video)
 })
--- pizza.js
+++ pizza.js
@@ -1,55 +1,52 @@
-const btn1 = document.querySelector('.btn1')
+const eventBriteApi = 'https://www.eventbriteapi.com/v3/events/120067319585/?token=BZU2AGMQK7AESWXYITBJ'
 
-document.addEventListener('DOMContentLoaded', function(event){
-    // code to load iframe when page loads
-    let iFrame = document.createElement('iframe');
 
-    
+async function loadEventBrite () {
+    let iFrame = document.createElement('iframe');  
     iFrame.setAttribute('id','iFrame');
     iFrame.height = '600'
     iFrame.width = '600'
     let divIframe = document.querySelector('#divIframe')
     divIframe.appendChild(iFrame)
-    // create prevent defeault
-    event.preventDefault();
-    //fetch data for eventbrite api to put in iframe
-    fetch('https://www.eventbriteapi.com/v3/events/120067319585/?token=BZU2AGMQK7AESWXYITBJ')
-    // return api response to a json
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data.url)
-        const link = data.url
-        iFrame.setAttribute('src',link)
-    })
-    
+    const response = await fetch(eventBriteApi)
+    const data = await response.json()
+    const link = await data.url
+    iFrame.setAttribute('src', link)
+    return data;
+}
 
-})
+async function loadVideoData () {
+    const response1 = await fetch (eventBriteApi);
+    const data = await response1.json();
+    const youtubeData = `https://www.googleapis.com/youtube/v3/search/?part=snippet&q=${data.name.text}&key=AIzaSyA4oLid5kOieJZhkWcUb2zc0hM3t_vjusA
+`
+    const response2 = await fetch(youtubeData);
+    const data2 = await response2.json();
+    const video = data2.items[0];
+    return video;
+}
+
+function addVideo (video) {
+    let videoFrame = document.createElement('iframe')
+    videoFrame.setAttribute('id','iFrame');
+    videoFrame.height = '600'
+    videoFrame.width = '600'
+    let divIframe2 = document.querySelector('#divIframe2')
+    divIframe2.height = '400'
+    divIframe2.width = '400'
+    divIframe2.appendChild(videoFrame)
+    let youTube = `https://www.youtube.com/embed/${video.id.videoId}`
+    videoFrame.setAttribute('src',youTube)
+    let p1 = document.createElement('p');           
+    let titleDescrip = video.snippet.description             
+    p1.textContent = titleDescrip              
+    divIframe2.appendChild(p1)
+}
 
 
 
-btn1.addEventListener('click', function(event){
-    // create prevent defeault
-    event.preventDefault();
-    
-
-
-    fetch('https://www.eventbriteapi.com/v3/events/120067319585/?token=BZU2AGMQK7AESWXYITBJ')
-        .then(function(response){
-            return response.json()
-        })
-        .then(function(data){ 
-            console.log(data.name.text)
-            
-            fetch(`https://www.googleapis.com/youtube/v3/search/?part=snippet&q=${data.name.text}&key=AIzaSyBw7ldeQl94AT_dRmGV5mWjupsyaq2pBfI`)
-            .then(function(response){
-                return response.json()
-            })
-            .then(function(data){
-                data.items.forEach(video => {
-                    console.log(video.id.videoId)
-                });
-            })
-        })
+document.addEventListener('DOMContentLoaded', async () => {
+    loadEventBrite()
+    const video = await loadVideoData()
+    addVideo(video)
 })
